b0a713f06c1afff081dc1cb0b40575884ad7b3a8
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-05-04 11:34:40 -0300

    feat(database): add new columns to classes and methods tables

--- crawler/database.py
+++ crawler/database.py
@@ -1,20 +1,26 @@
-import sqlite3
-
 def create_tables(conn):
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS classes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
-            file_path TEXT
+            file_path TEXT,
+            line_number INTEGER,
+            superclass TEXT,
+            interfaces TEXT,
+            class_type TEXT
         )
     ''')
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS methods (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            class_name TEXT,
+            class_id INTEGER,
             method_name TEXT,
-            line_number INTEGER
+            line_number INTEGER,
+            return_type TEXT,
+            modifier TEXT,
+            is_static BOOLEAN,
+            FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE
         )
     ''')
     conn.commit()
@@ -22,15 +28,48 @@ def create_tables(conn):
 def insert_class(conn, class_info):
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO classes (name, file_path)
-        VALUES (?, ?)
-    ''', (class_info.name, class_info.file_path))
+        INSERT INTO classes (name,
+                             file_path,
+                             line_number,
+                             superclass,
+                             interfaces,
+                             class_type)
+        VALUES (?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?)
+    ''', (class_info.name,
+          class_info.file_path,
+          class_info.line_number,
+          class_info.superclass,
+          class_info.interfaces,
+          class_info.class_type)
+    )
     conn.commit()
+    return cursor.lastrowid
 
 def insert_method(conn, method_info):
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO methods (class_name, method_name, line_number)
-        VALUES (?, ?, ?)
-    ''', (method_info.class_name, method_info.method_name, method_info.line_number))
+        INSERT INTO methods (class_id,
+                             method_name,
+                             line_number,
+                             return_type,
+                             modifier,
+                             is_static)
+        VALUES (?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?)
+    ''', (method_info.class_id,
+          method_info.method_name,
+          method_info.line_number,
+          method_info.return_type,
+          method_info.modifier,
+          method_info.is_static)
+    )
     conn.commit()
