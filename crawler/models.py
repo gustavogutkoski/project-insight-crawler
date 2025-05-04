ed08c4204f852230b8405cd66a49e9f61eeb8406
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-05-04 10:17:56 -0300

    refactor(models): add new attributes to ClassInfo and MethodInfo dataclasses

--- crawler/models.py
+++ crawler/models.py
@@ -1,12 +1,20 @@
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class ClassInfo:
     name: str
     file_path: str
+    line_number: int
+    superclass: Optional[str] = None
+    interfaces: Optional[str] = None
+    class_type: str = "class"
 
 @dataclass
 class MethodInfo:
     class_name: str
     method_name: str
     line_number: int
+    return_type: Optional[str] = None
+    modifier: Optional[str] = None
+    is_static: bool = False
